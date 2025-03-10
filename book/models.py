05d4bee24295b9de1612c8c1c6370fcc853f0331
Ayyadurai <kmayyadurai286@gmail.com>
2025-03-10 21:12:20 +0530

    Initialize Django project with Book app and configure .gitignore

---
+++ book/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+"""
+Book - Model
+name -> field
+CharField -> Field Type
+Inside parentheses - attributes or constraints
+"""
+
+
+# Create your models here.
+class Book(models.Model):
+    name = models.CharField(
+        max_length=30
+    )  # Use char field when you want to specify tbe length
+    description = (
+        models.TextField()
+    )  # Use text field when you dont want to specify tbe length
+
+
+# TO CONVERT MODEL INTO TABLE
+
+#1. python manage.py makemigrations
+#2.  python manage.py migrate
