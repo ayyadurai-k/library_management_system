5b9a659017050c2e460c8513bd8ba9e4afd4692d
Ayyadurai <kmayyadurai286@gmail.com>
2025-03-17 21:07:14 +0530

    Added API to create book

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from book.views import list_books,get_book
+from book.views import list_books,get_book,create_book
 
 
 urlpatterns = [
+    path("create/",create_book),
     path("list/",list_books),
     path("get/<int:book_id>/",get_book)
 ]
--- book/views.py
+++ book/views.py
@@ -1,9 +1,22 @@
+from asyncio import IocpProactor
 from django.shortcuts import render
 from book.models import Book
 from django.http import JsonResponse
-
-
+import json 
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+@csrf_exempt
+def create_book(request):
+    data = json.loads(request.body) # CONVERT JSON INTO DICTIONARY
+    
+    name = data.get("name") # EXTRACT VALUE FROM DICTIONARY
+    description = data.get("description") # EXTRACT VALUE FROM DICTIONARY
+    
+    book = Book.objects.create(name=name,description=description)
+    
+    return JsonResponse({"message": "Book created successfully"})
+
 def list_books(request):
     books = Book.objects.all().values("id","name", "description")
     return JsonResponse(list(books), safe=False)
@@ -17,4 +30,4 @@ def get_book(request,book_id):
         "description": book.description
     }
     return JsonResponse(data,safe=False)
-    
+
