0ae365eeaefb1724c1118eae1a83e4985acb6fea
Ayyadurai <kmayyadurai286@gmail.com>
2025-03-25 20:44:53 +0530

    `Added update_book view and corresponding URL pattern`

--- book/urls.py
+++ book/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from book.views import get_book, list_books, create_book
+from book.views import get_book, list_books, create_book,update_book
 
 
 urlpatterns = [
     path("get/<int:book_id>/", get_book),
     path("list/", list_books),
     path("create/", create_book),
+    path("update/<int:book_id>/", update_book),
 ]
--- book/views.py
+++ book/views.py
@@ -4,15 +4,16 @@ from book.models import Book
 from book.serializers import BookSerializer
 from rest_framework.response import Response
 
-@api_view(["GET"]) # TO SPECIFY THE REQUEST METHOD
-def get_book(request,book_id):
+
+@api_view(["GET"])  # TO SPECIFY THE REQUEST METHOD
+def get_book(request, book_id):
     book = Book.objects.get(id=book_id)
-    serializer = BookSerializer(book) # CONVERT MODEL OBJECT INTO JSON
-    return Response(serializer.data) # SEND RESPONSE TO USER
+    serializer = BookSerializer(book)  # CONVERT MODEL OBJECT INTO JSON
+    return Response(serializer.data)  # SEND RESPONSE TO USER
 
 
 @api_view(["GET"])
-def list_books(request):    
+def list_books(request):
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
@@ -23,4 +24,13 @@ def create_book(request):
     serializer = BookSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
-    return Response({"message":"Book created successfully"}, status=201)
+    return Response({"message": "Book created successfully"}, status=201)
+
+
+@api_view(["PATCH"])
+def update_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    serializer = BookSerializer(instance=book, data=request.data, partial=True)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response({"message": "Book updated successfully"}, status=200)
