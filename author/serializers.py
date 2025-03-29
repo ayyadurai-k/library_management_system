1f01d1b961834cbff86c0270d224ee1cc712902d
Ayyadurai <kmayyadurai286@gmail.com>
2025-03-29 21:10:30 +0530

    `Added validation for age in AuthorSerializer and corrected field definition in Meta class.`

--- author/serializers.py
+++ author/serializers.py
@@ -5,4 +5,13 @@ from rest_framework import serializers
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = '__all__'
+        fields = "__all__"
+
+    def validate(self, data):
+        if data["age"] < 20 or data["age"] > 60:
+            raise serializers.ValidationError(
+                "Age should be greater 20 and less than or equal to 60"
+            )
+        
+        
+        return data
