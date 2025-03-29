b596a186a73e38b95fc1d48eaac85cde5216cc82
Ayyadurai <kmayyadurai286@gmail.com>
2025-03-29 21:13:15 +0530

    Removed unused import of status from author/views.py

--- author/views.py
+++ author/views.py
@@ -1,6 +1,5 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
 from author.models import Author
 from author.serializers import AuthorSerializer
 
